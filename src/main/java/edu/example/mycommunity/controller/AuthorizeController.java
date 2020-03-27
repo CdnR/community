5c27cb56a49b155d4f099457bc01f3fc800fa9f4
CdnR <wangyuaqin@qq.com>
2020-03-28 00:40:50 +0800

    sed commit

--- src/main/java/edu/example/mycommunity/controller/AuthorizeController.java
+++ src/main/java/edu/example/mycommunity/controller/AuthorizeController.java
@@ -2,30 +2,64 @@ package edu.example.mycommunity.controller;
 
 import edu.example.mycommunity.dto.GithubUesr;
 import edu.example.mycommunity.dto.accessTokenDTO;
+import edu.example.mycommunity.mapper.UserMapper;
+import edu.example.mycommunity.model.User;
 import edu.example.mycommunity.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
 @Controller
 public class AuthorizeController {
+
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String secret;
+
+    @Value("${github.redirect_uri}")
+    private String redirectUri;
 
     @Autowired
     private GithubProvider githubProvider;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")String code,
-                           @RequestParam(name = "state")String state){
+                           @RequestParam(name = "state")String state,
+                           HttpServletResponse response){
         accessTokenDTO accessTokenDTO = new accessTokenDTO();
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setClient_id("1984ccc4cf0af8fc304e");
-        accessTokenDTO.setClient_secret("626c8a72a734ecd6547028d6ea782382127d10aa");
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(secret);
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUesr user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUesr githubUesr = githubProvider.getUser(accessToken);
+        if(githubUesr!=null){
+        //登录成功，写cookie和session
+            User user = new User();
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
+            user.setName(githubUesr.getName());
+            user.setAccountId(String.valueOf(githubUesr.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            response.addCookie(new Cookie("token",token));
+            return "redirect:/";
+        }else {
+            return "redirect:/";
+        }
+
     }
 }
