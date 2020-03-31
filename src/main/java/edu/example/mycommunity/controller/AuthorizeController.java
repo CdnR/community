d64a2d3de240aa13571e8684b80e414008f6623d
CdnR <wangyuaqin@qq.com>
2020-04-01 01:05:48 +0800

    five commit

--- src/main/java/edu/example/mycommunity/controller/AuthorizeController.java
+++ src/main/java/edu/example/mycommunity/controller/AuthorizeController.java
@@ -54,6 +54,7 @@ public class AuthorizeController {
             user.setAccountId(String.valueOf(githubUesr.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
+            user.setAvatarUrl(githubUesr.getAvatar_url());
             userMapper.insert(user);
             response.addCookie(new Cookie("token",token));
             return "redirect:/";
--- src/main/java/edu/example/mycommunity/model/Question.java
+++ src/main/java/edu/example/mycommunity/model/Question.java
@@ -1,5 +1,8 @@
 package edu.example.mycommunity.model;
 
+import lombok.Data;
+
+@Data
 public class Question {
     private Integer id;
     private String title;
@@ -12,83 +15,5 @@ public class Question {
     private Integer commrntCount;
     private Integer likeCount;
 
-    public Integer getId() {
-        return id;
-    }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getTag() {
-        return tag;
-    }
-
-    public void setTag(String tag) {
-        this.tag = tag;
-    }
-
-    public long getGmtCreate() {
-        return gmtCreate;
-    }
-
-    public void setGmtCreate(long gmtCreate) {
-        this.gmtCreate = gmtCreate;
-    }
-
-    public long getGmtModified() {
-        return gmtModified;
-    }
-
-    public void setGmtModified(long gmtModified) {
-        this.gmtModified = gmtModified;
-    }
-
-    public Integer getCreator() {
-        return creator;
-    }
-
-    public void setCreator(Integer creator) {
-        this.creator = creator;
-    }
-
-    public Integer getViewCount() {
-        return viewCount;
-    }
-
-    public void setViewCount(Integer viewCount) {
-        this.viewCount = viewCount;
-    }
-
-    public Integer getCommrntCount() {
-        return commrntCount;
-    }
-
-    public void setCommrntCount(Integer commrntCount) {
-        this.commrntCount = commrntCount;
-    }
-
-    public Integer getLikeCount() {
-        return likeCount;
-    }
-
-    public void setLikeCount(Integer likeCount) {
-        this.likeCount = likeCount;
-    }
 }
